9ef4bc45ab6d844049d7041df87cfeea02e67e0f
Jason Herndon <jasonherndon86@gmail.com>
2016-11-23 15:01:05 -0700

    WIP Posts

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,13 +4,9 @@
 |--------------------------------------------------------------------------
 | Model Factories
 |--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
 */
 
+// Users
 $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     static $password;
 
@@ -34,4 +30,78 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'ip' => $faker->ipv4,
         'remember_token' => str_random(10),
     ];
+});
+
+// Posts
+$factory->define(App\Models\Post::class, function (Faker\Generator $faker) {
+
+    $title = $faker->sentence(6,true);
+
+    return [
+        'title' => $title,
+        'slug' => str_slug($title),
+        'content' => $faker->text,
+        'img' => $faker->imageUrl(800, 400, 'people', true, 'PlatoLMS', true),
+        'video' => $faker->text,
+    ];
+});
+
+// Pages
+$factory->define(App\Models\Page::class, function (Faker\Generator $faker) {
+
+    $title = $faker->sentence(6,true);
+
+    return [
+        'layout_id' => $faker->numberBetween(0,10),
+        'title' => $title,
+        'slug' => str_slug($title),
+        'content' => $faker->text,
+        'img' => $faker->imageUrl(800, 400, 'people', true, 'PlatoLMS', true),
+        'video' => $faker->text,
+    ];
+});
+
+// Categories
+$factory->define(App\Models\Category::class, function (Faker\Generator $faker) {
+    //
+});
+
+// Tags
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    //
+});
+
+// Tags
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    //
+});
+
+// Tags
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    //
+});
+
+// Tags
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    //
+});
+
+// Tags
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    //
+});
+
+// Tags
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    //
+});
+
+// Tags
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    //
+});
+
+// Tags
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    //
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Users
         $this->call(UsersTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersRolesTableSeeder::class);
+
+        // FE Site
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
+        $this->call(PostsCategoriesTableSeeder::class);
+        $this->call(PostsTagsTableSeeder::class);
+        $this->call(PagesTableSeeder::class);
+
+        // Courses
+
     }
 }
