4f38cc919de0a1f30e79f85417c3890a3471eee6
Jason Herndon <jasonherndon86@gmail.com>
2017-01-11 23:22:58 -0700

    Update config.settings with course levels
    Also - clean up formatting

--- config/settings.php
+++ config/settings.php
@@ -2,46 +2,40 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Text Settings
-    |--------------------------------------------------------------------------
-    */
+    'course' => [
+        /*
+        | Course Levels
+        |--------------------------------------------------------------------------
+        | Set the levels you wish to use for organizing courses. Leave the array empty
+        | to disregard coures levels.
+        */
+        'levels' => [
+            '100', '200', '300', '400', '500', '600'
+        ],
 
+    ],
     'user' => [
         /*
-        |--------------------------------------------------------------------------
         | User Image Policy
         |--------------------------------------------------------------------------
-        |
-        | If you fill in this text string, a box will appear near any prompt for a
-        | user to upload their photo. It's a good place for a reminder about any
-        | rules you may have regarding content rules, privacy, or security.
-        |
+        | If you fill in this string, the text will appear near any prompt for users
+        | to upload their photo - great for reminders re: content, privacy, or security.
         */
         'user_image_policy' => '',
 
         /*
-        |--------------------------------------------------------------------------
         | User Image Resize
         |--------------------------------------------------------------------------
-        |
-        | Here you can customize the pixel value that will be used when resizing
-        | profile images.
-        |
+        | Here you can customize the pixel value \used when resizing profile images.
         */
         'user_image_resize' => 250,
 
         /*
-        |--------------------------------------------------------------------------
         | User Archive Limit (in Days)
         |--------------------------------------------------------------------------
-        |
-        | Here, you can customize the number of days that you wish to keep the
-        | records of deleted student accounts. At the end of this interval,
-        | any student accounts in the trash, and their data will be moved 
-        | out of the system for cold storage.
-        |
+        | Sets the number of days that you wish to keep the records of deleted student 
+        | accounts. At the end of this interval, any student accounts in the trash, 
+        | and their data will be zipped and archived. Mark as -1 to keep indefinitely.
         */
         'user_archive_limit' => 30,    
     ],
